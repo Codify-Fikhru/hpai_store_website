917ad44026326e65e7e0051f111ca2cc651d5924
Codify-Fikhru <jokiofficialyt@gmail.com>
2025-01-05 18:07:36 +0800

    initial u[date this product

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./public/**/*.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
